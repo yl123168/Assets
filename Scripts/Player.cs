aa3a3c3eb1a9f185ea88d7bb9dfde93745f00b71
yl123168 <724162642@qq.com>
2019-12-28 20:46:39 +0800

    Update Player.cs

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -7,5 +7,17 @@ using UnityEngine;
 /// </summary>
 public class Player : MonoBehaviour
 {
-    
+    private float horizon;
+    private float vertical;
+    public float moveSpeed=5;
+
+    private void Update()
+    {
+        horizon = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        if (horizon != 0 || vertical != 0)
+        {
+            this.transform.Translate(horizon*moveSpeed*Time.deltaTime, vertical*moveSpeed*Time.deltaTime, 0,Space.World);
+        }
+    }
 }
