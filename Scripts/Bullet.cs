35c7a31613868544db17d36f2bedd9a2d83602e6
yl123168 <724162642@qq.com>
2020-01-05 20:51:20 +0800

    Create Bullet.cs

---
+++ Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 子弹、运动触发检测
+/// </summary>
+public class Bullet : MonoBehaviour
+{
+    /// <summary>
+    /// 子弹飞行速度
+    /// </summary>
+    public float bulletSpeed = 50;
+
+    private void Update()
+    {
+        BulletMove();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+    private void BulletMove() {
+        this.transform.Translate(this.transform.up*bulletSpeed*Time.deltaTime,Space.World);
+    }
+
+}
