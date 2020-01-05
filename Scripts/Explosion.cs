82906b4349636885386f2e82e5c5df08cfa09cc3
yl123168 <724162642@qq.com>
2020-01-05 22:17:51 +0800

    Create Explosion.cs

---
+++ Scripts/Explosion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class Explosion : MonoBehaviour
+{
+    public float ExplosionTime = 1f;
+    private void Start()
+    {
+        Destroy(this.gameObject, ExplosionTime);
+    }
+}
