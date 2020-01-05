94135b7224b92168fd759aa24a8a164d99326ab5
yl123168 <724162642@qq.com>
2020-01-05 22:17:49 +0800

    Update Bullet.cs

--- Scripts/Bullet.cs
+++ Scripts/Bullet.cs
@@ -20,6 +20,9 @@ public class Bullet : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         switch (collision.tag) {
+            case "Player":
+                collision.GetComponent<Player>().Death();
+                break;
             case  "Wall":
                 break;
             case "Grass":
